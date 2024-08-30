2d437bb73f456c0dfbaa3bf18fc9065c60568a30
Margaux <margaux.espinasse@gmail.com>
2024-08-30 09:09:45 +0200

    Updated CORS config and allowed origins

--- index.js
+++ index.js
@@ -10,29 +10,27 @@ require('./passport.js');
 const app = express();
 const { check, validationResult } = require('express-validator');
 
-// CORS to update
-let allowedOrigins = [
-  'http://localhost:8080',
-  'https://movies-app2024-74d588eb4f3d.herokuapp.com/',
-];
+// restricted origins
+// let allowedOrigins = [
+//   'http://localhost:8080',
+//   'https://movies-app2024-74d588eb4f3d.herokuapp.com/',
+// ];
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        // If a specific origin isn’t found on the list of allowed origins
-        let message =
-          'The CORS policy for this application doesn’t allow access from origin ' +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true);
+//         let message =
+//           'The CORS policy for this application doesn’t allow access from origin ' +
+//           origin;
+//         return callback(new Error(message), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
 
-// end of CORS
+app.use(cors()); // Allows requests from all origins
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -182,7 +180,7 @@ app.delete(
   async (req, res) => {
     try {
       const updatedUser = await User.findOneAndUpdate(
-        req.params.id,
+        req.params.Username,
         { $pull: { FavoriteMovies: req.params.MovieID } },
         { new: true }
       );
