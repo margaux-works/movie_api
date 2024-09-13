5bc39f3c73b1f59ee29b0a3fc1aab799527a4fca
Margaux <margaux.espinasse@gmail.com>
2024-09-13 14:59:20 +0200

    Update get movie by id endpoint

--- index.js
+++ index.js
@@ -263,7 +263,7 @@ app.get(
   '/movies/:id',
   passport.authenticate('jwt', { session: false }),
   async (req, res) => {
-    await Movie.findOne({ _id: req.params.id })
+    await Movie.findOne({ _id: req.params.id.toString() })
       .then((movie) => {
         res.json(movie);
       })
