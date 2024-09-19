dec18055f3c01f398ff3fb2907cb22886abe5547
Margaux <margaux.espinasse@gmail.com>
2024-09-19 18:15:09 +0200

    updated cors

--- index.js
+++ index.js
@@ -11,29 +11,29 @@ const app = express();
 const { check, validationResult } = require('express-validator');
 
 // restricted origins
-let allowedOrigins = [
-  'http://localhost:8080',
-  'http://localhost:1234',
-  'https://movies-app2024-74d588eb4f3d.herokuapp.com',
-  'https://margauxflix.netlify.app',
-];
+// let allowedOrigins = [
+//   'http://localhost:8080',
+//   'http://localhost:1234',
+//   'https://movies-app2024-74d588eb4f3d.herokuapp.com',
+//   'https://margauxflix.netlify.app',
+// ];
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        let message =
-          'The CORS policy for this application doesn’t allow access from origin ' +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         let message =
+//           'The CORS policy for this application doesn’t allow access from origin ' +
+//           origin;
+//         return callback(new Error(message), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
 
-// app.use(cors()); // Allows requests from all origins
+app.use(cors()); // Allows requests from all origins
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
