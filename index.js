23f92e45ac1248b32b9643b899436c2ec14a7293
Margaux <margaux.espinasse@gmail.com>
2024-09-19 17:38:04 +0200

    Update cors policy

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const { check, validationResult } = require('express-validator');
 // restricted origins
 let allowedOrigins = [
   'http://localhost:8080',
-  'https://movies-app2024-74d588eb4f3d.herokuapp.com/',
+  'https://movies-app2024-74d588eb4f3d.herokuapp.com',
   'https://margauxflix.netlify.app',
 ];
 
